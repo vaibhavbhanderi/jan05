448751b58362f777d3bc4a7e596a6509848ffaaf
powerofpatel11 <vaibhavbhanderi5@gmail.com>
2023-01-05 21:55:07 +0530

    ok

--- router/router.js
+++ router/router.js
@@ -7,11 +7,15 @@ router.post("/login", async (req, resp) => {
     const password = req.body.password;
     console.log(name + " " + password);
     const result1 = await Student.findOne({ name: name });
+    if(!result1){
+        resp.send("invalid credentials");
+        return;
+    }
     console.log(result1.password);
     // const isvalid = await bcrypt.compare(password, result1.password);
 
     // console.log(isvalid);
-    if (password !== result1.password) {
+    if (password !== result1.password ) {
       resp.send("invalid credentials");
       return;
     }
